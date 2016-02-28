0b095eee4774aea2bc2fb210a5a0bca30e272672
Samir Talwar <samir@noodlesandwich.com>
2016-02-28 16:40:06 +0000

    Run the application on port 8080.

--- api/src/main/scala/com/bemorerandom/api/ApiServer.scala
+++ api/src/main/scala/com/bemorerandom/api/ApiServer.scala
@@ -5,7 +5,11 @@ import com.twitter.finatra.http.HttpServer
 import com.twitter.finatra.http.routing.HttpRouter
 import com.twitter.finatra.json.modules.FinatraJacksonModule
 
+object ApiServerMain extends ApiServer
+
 class ApiServer extends HttpServer {
+  override val defaultFinatraHttpPort: String = ":8080"
+
   override def jacksonModule = ApiJacksonModule
 
   override protected def configureHttp(router: HttpRouter): Unit = {

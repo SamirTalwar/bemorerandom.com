953cd32a32eec5f3ce98acba4acaf85364dfc977
Samir Talwar <samir@noodlesandwich.com>
2016-03-02 22:39:11 +0000

    Configure the application to talk to a database.

---
+++ api/src/main/scala/com/bemorerandom/api/Config.scala
@@ -0,0 +1,16 @@
+package com.bemorerandom.api
+
+import scala.collection.JavaConverters._
+
+case class Config(database: Config.Database)
+
+object Config {
+  case class Database(url: String, user: String, password: String)
+
+  def fromEnvironmentVariables() = {
+    val env = System.getenv().asScala
+    Config(
+      database = Config.Database(url = env("DB_URL"), user = env("DB_USER"), password = env("DB_PASSWORD"))
+    )
+  }
+}
